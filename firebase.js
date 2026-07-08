06cea9824f7dd0b2e9e32747fddb12cfccfa8f04
kumarshiva74325-cell <kumarshiva74325@gmail.com>
2026-07-08 11:24:38 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,73 +1,44 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+// Firebase SDK
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import {
+getAuth,
+createUserWithEmailAndPassword,
+signInWithEmailAndPassword,
+signOut,
+onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
 import {
-  getAuth,
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-  signOut,
-  onAuthStateChanged
-} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+getFirestore,
+collection,
+addDoc,
+getDocs,
+deleteDoc,
+doc
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCVuG68S7GBYxCgu4lAx5HNl4lbx5nfinw",
-  authDomain: "datashare-6e29b.firebaseapp.com",
-  projectId: "datashare-6e29b",
-  storageBucket: "datashare-6e29b.firebasestorage.app",
-  messagingSenderId: "918210510065",
-  appId: "1:918210510065:web:10b3aab63af97699c1bd50",
-  measurementId: "G-ET4K4P797B"
+apiKey: "YOUR_API_KEY",
+authDomain: "YOUR_PROJECT.firebaseapp.com",
+projectId: "YOUR_PROJECT_ID",
+storageBucket: "YOUR_PROJECT.appspot.com",
+messagingSenderId: "YOUR_SENDER_ID",
+appId: "YOUR_APP_ID"
 };
 
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
 
-window.signup = async function () {
+export const auth = getAuth(app);
+export const db = getFirestore(app);
 
-  const email = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
-
-  try {
-    await createUserWithEmailAndPassword(auth, email, password);
-    alert("Signup Successful");
-  } catch (error) {
-    alert(error.message);
-  }
-
+export {
+createUserWithEmailAndPassword,
+signInWithEmailAndPassword,
+signOut,
+onAuthStateChanged,
+collection,
+addDoc,
+getDocs,
+deleteDoc,
+doc
 };
-
-window.login = async function () {
-
-  const email = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
-
-  try {
-    await signInWithEmailAndPassword(auth, email, password);
-    alert("Login Successful");
-  } catch (error) {
-    alert(error.message);
-  }
-
-};
-
-window.logout = async function () {
-
-  try {
-    await signOut(auth);
-    alert("Logged Out");
-  } catch (error) {
-    alert(error.message);
-  }
-
-};
-
-onAuthStateChanged(auth, (user) => {
-
-  const currentUser = document.getElementById("currentUser");
-
-  if (user) {
-    currentUser.innerHTML = "Logged in as: <b>" + user.email + "</b>";
-  } else {
-    currentUser.innerHTML = "Not Logged In";
-  }
-
-});
